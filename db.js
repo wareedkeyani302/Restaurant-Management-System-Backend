9bc76645e1b9fbdfe44ed8c14f0fbcd5fa0ce576
wareed.sheraz <wareed.keyani@outloo.com>
2024-09-03 11:22:27 +0500

    Update db.js and package files

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '123456',
-    database: 'restaurant_management_system',
+    host: process.env.DB_Host,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10),
     queueLimit: 0
 })
 
